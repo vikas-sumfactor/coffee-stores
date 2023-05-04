4eb31e4724ee701f1604b938c3990bdaf728b0e0
vikas-sumfactor <vikas.sumfactor@gmail.com>
2023-05-04 16:34:34 +0530

    first commit

---
+++ pages/coffestore/[id].tsx
@@ -0,0 +1,21 @@
+
+import { useRouter } from "next/router";
+import Link from "next/link";
+
+
+const CoffeeStore = () => {
+  const router = useRouter();
+  console.log("router", router);
+  return (
+    <div>
+      Coffee Store Page {router.query.id}
+      <Link href="/" legacyBehavior>
+        <a>Back to home</a>
+      </Link>
+      <Link href="/coffee-store/dynamic" legacyBehavior >
+        <a>Go to page dynamic</a>
+      </Link>
+    </div>
+  );
+};
+export default CoffeeStore;
